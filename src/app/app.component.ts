3c2d8e55e2e7ec532af85c1bde5020ed0f47f553
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2019-11-17 03:16:33 +0100

    ng-for objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,43 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'proyectoAngular';
   items = ['nicolas', 'julian', 'perez'];
+  products : Product[] = [
+    {
+      id: '1',
+      imageURL: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 50.00,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      imageURL: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 30.00,
+      description: 'bla bla bla bla'
+    },
+    {
+      id: '3',
+      imageURL: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 30.00,
+      description: 'bla bla bla bla'
+    },
+    {
+      id: '4',
+      imageURL: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 30.00,
+      description: 'bla bla bla bla'
+    },
+    {
+      id: '5',
+      imageURL: 'assets/images/stickers1.png',
+      title: 'Stickers 1',
+      price: 30.00,
+      description: 'bla bla bla bla'
+    }
+  ];
 
   addItem(){
     this.items.push('Nuevo item');
