27b6d6403ecee571cbb08c7db2134bbb3df3f813
masterfabela <fabelaretorcida@gmail.com>
2019-12-22 03:24:01 +0000

    Eventos de componente

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,13 +1,46 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy
+} from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<string> = new EventEmitter();
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  /*
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('on change');
+    console.log(changes);
+  }
+  */
+
+  ngOnInit() {
+    console.log('on init');
+  }
+
+  ngDoCheck() {
+    console.log('do check');
+  }
+
+  ngOnDestroy() {
+    console.log('destroy');
+  }
 
   addCart() {
     console.log('añadir al carrito');
